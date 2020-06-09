a3b350ffd2279973c842dd35aac2f5343856527b
Ces <cesmejia@gruposeara.com>
2020-06-08 19:23:47 -0500

    update sh file

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo 'hello Totemp'
+echo 'hello Totemp_ter'
 
-cd /home/pi/Documents/totemp/thermo_box
+cd /home/pi/Documents/totemp_ter/thermo_box
 lxterminal -e "python3 thermo_box.py" &
 
-cd /home/pi/Documents/totemp/temp-rfid-ternium
+cd /home/pi/Documents/totemp_ter/temp-rfid-ternium
 lxterminal -e "npm run server" &
 # lxterminal -e "serve -s build" &
 
-cd /home/pi/Documents/totemp/temp-rfid-ternium
+cd /home/pi/Documents/totemp_ter/temp-rfid-ternium
 # lxterminal -e "npm run start" &
 lxterminal -e "serve -s build -l 3000" &
 
@@ -21,6 +21,6 @@ chromium-browser --kiosk --app=http://localhost:3000
 # to autorun file on start 
 # cd /etc/xdg/lxsession/LXDE-pi/
 # sudo nano autostart
-# @/home/pi/Documents/totemp/temp-rfid-ternium/start.sh
+# @/home/pi/Documents/totemp_ter/temp-rfid-ternium/start.sh
 # CTRL O , ENTER
 # CTRL X
